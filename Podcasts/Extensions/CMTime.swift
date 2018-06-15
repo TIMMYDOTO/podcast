f05ee047959eb2116ad6722b852928241229a48b
Ivan Amidžić <iamidzic92@gmail.com>
2018-06-15 18:33:15 +0200

    fixed duration minutes bug

--- Podcasts/Extensions/CMTime.swift
+++ Podcasts/Extensions/CMTime.swift
@@ -18,7 +18,7 @@ extension CMTime {
         
         let totalSeconds = Int(CMTimeGetSeconds(self))
         let seconds = totalSeconds % 60
-        let minutes = totalSeconds / 60
+        let minutes = totalSeconds % (60 * 60) / 60
         let hours = totalSeconds / 60 / 60
         let timeFormatString = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
         return timeFormatString
